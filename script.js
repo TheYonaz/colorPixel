cda9c18100659cec75007e82dc189a4a19c1ef0f
TheYonaz <117009178+TheYonaz@users.noreply.github.com>
2022-12-31 14:10:58 +0200

    color picker

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-console.log(5);
+let colors = document.getElementsByClassName("color");
 
-let i = 8;
-i = 9;
-console.log(i);
+for (let i of colors) {
+  i.addEventListener("click", (e) => {
+    setActiveColor(e.target.textContent);
+  });
+}
+
+let activeColorPaint = "";
+
+function setActiveColor(color) {
+  console.log(color);
+  let activeColor = document.getElementById("active-color");
+  activeColor.innerText = color;
+  activeColor.style = `color: ${color}`;
+  activeColorPaint = color;
+}
