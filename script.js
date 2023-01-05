4fcf6d151abfb3411a4cd1dbe56ff82b8b2080e4
TheYonaz <117009178+TheYonaz@users.noreply.github.com>
2023-01-05 21:50:04 +0200

    updatred

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
+let drag = "false";
 let colors = document.getElementsByClassName("color");
+let container = document.getElementById("grid-container");
+let num = 16;
+let totalNum = num * num;
+container.style.gridTemplateColumns = `repeat(${num} 1fr)`;
+container.style.gridTemplateRows = `repeat(${num} 1fr)`;
+
+document.addEventListener("mousedown", () => (drag = "true"));
+document.addEventListener("mouseup", () => (drag = "false"));
+
+for (i = 0; i < totalNum; i++) {
+  const box = document.createElement("div");
+  box.classList.add("grid-item");
+  container.insertAdjacentElement("beforeend", box);
+  if ((drag = "true")) {
+    box.addEventListener("mouseover", changeColor);
+  } else {
+    box.addEventListener("click", changeColor);
+  }
+}
 
 for (let i of colors) {
   i.addEventListener("click", (e) => {
@@ -9,9 +29,11 @@ for (let i of colors) {
 let activeColorPaint = "";
 
 function setActiveColor(color) {
-  console.log(color);
   let activeColor = document.getElementById("active-color");
   activeColor.innerText = color;
   activeColor.style = `color: ${color}`;
   activeColorPaint = color;
+}
+function changeColor() {
+  this.style = `background-color: ${activeColorPaint}`;
 }
